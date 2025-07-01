7b1ce4460cd4d9244d62e30fdbc74788a35f0e1b
Manju <manju.k200456@gmail.com>
2025-07-01 19:23:09 +0530

    Add files via upload

---
+++ Task5/Task5.sql
@@ -0,0 +1,9 @@
+SELECT StudentID,Name,(MathScore + ScienceScore + EnglishScore) AS TotalScore,CASE WHEN (MathScore + ScienceScore + EnglishScore) 
+ >= 270 THEN 'A'WHEN (MathScore + ScienceScore + EnglishScore) >= 240 THEN 'B' WHEN (MathScore + ScienceScore + EnglishScore)
+ >= 210 THEN 'C' ELSE 'D (Fail)' END AS AssignedGrade FROM Students;
+ 
+ SELECT StudentID,Name,MathScore,CASE WHEN MathScore >= 40 THEN 'Pass' ELSE 'Fail' END AS MathStatus,ScienceScore, CASE 
+  WHEN ScienceScore >= 40 THEN 'Pass' ELSE 'Fail' END AS ScienceStatus, EnglishScore, CASE WHEN EnglishScore >= 40 THEN 'Pass' ELSE
+ 'Fail' END AS EnglishStatus FROM Students;
+
+ 
