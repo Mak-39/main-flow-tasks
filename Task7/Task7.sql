48b5e8cceee7be448fd13aebce8eaaabd1c539c0
Manju <manju.k200456@gmail.com>
2025-07-20 10:13:08 +0530

    Delete Task7 directory

--- Task7/Task7.sql
+++
@@ -1,9 +0,0 @@
-use studentmanagement;
-CREATE VIEW Studentscores AS SELECT StudentID, Name,'Math' AS Subject, MathScore AS Score FROM Students UNION SELECT StudentID,
- Name,'Science' AS Subject, ScienceScore AS Score FROM Students UNION SELECT StudentID, Name,'English' AS Subject,
-EnglishScore AS Score FROM Students;
-SELECT * FROM Studentscores;
-CREATE VIEW passedstudents AS SELECT StudentID, Name FROM Students WHERE MathScore >= 40 AND ScienceScore >= 40 AND
-EnglishScore >= 40;
-SELECT * FROM passedstudents;
-
